7b6697acf154aeb9074d733cf7b5d2c9cb3debc0
Dominik Meißner <dominik.meissner@uni-ulm.de>
2019-09-19 19:00:50 +0200

    fix linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    L: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-  },
+    'no-console': 'off'
+  }
 };
--- src/L.GBFS.js
+++ src/L.GBFS.js
@@ -1,131 +1,132 @@
 import './L.GBFS.css';
 
 L.GBFS = L.Layer.extend({
-    options: {
-        gbfsURL: '',
-        start: true,
-        interval: 60 * 1000,
-        onlyRunWhenAdded: false,
-        bikeMarkerColor: "rgb(87, 162, 255)",
-        bikeMarkerBgColor: "white"
-    },
+  options: {
+    gbfsURL: '',
+    start: true,
+    interval: 60 * 1000,
+    onlyRunWhenAdded: false,
+    bikeMarkerColor: 'rgb(87, 162, 255)',
+    bikeMarkerBgColor: 'white',
+  },
 
-    initialize: function(options) {
-        L.setOptions(this, options);
-        this.container = L.geoJson(null, options);
+  initialize(options) {
+    L.setOptions(this, options);
+    this.container = L.geoJson(null, options);
 
-        if (this.options.start && !this.options.onlyRunWhenAdded) {
-            this.start();
-        }
-    },
+    if (this.options.start && !this.options.onlyRunWhenAdded) {
+      this.start();
+    }
+  },
 
-    start() {
-        if (!this.timer) {
-            this.timer = setInterval(() => this.update(), this.options.interval);
-            this.update();
-        }
+  start() {
+    if (!this.timer) {
+      this.timer = setInterval(() => this.update(), this.options.interval);
+      this.update();
+    }
 
-        return this;
-    },
+    return this;
+  },
 
-    stop() {
-        if (this.timer) {
-            clearTimeout(this.timer);
-            delete this.timer;
-        }
+  stop() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+      delete this.timer;
+    }
 
-        return this;
-    },
+    return this;
+  },
 
-    isRunning() {
-        return this.timer !== undefined;
-    },
-    
-    async update() {
-        try {
-            const stationInformationResponse = await fetch(`${this.options.gbfsURL}station_information.json`);
-            const stations = await stationInformationResponse.json();
-            const stationStatusResponse = await fetch(`${this.options.gbfsURL}station_status.json`);
-            const stationStatus = await stationStatusResponse.json();
+  isRunning() {
+    return this.timer !== undefined;
+  },
 
-            this.container.clearLayers();
+  async update() {
+    try {
+      const stationInformationResponse = await fetch(`${this.options.gbfsURL}station_information.json`);
+      const stations = await stationInformationResponse.json();
+      const stationStatusResponse = await fetch(`${this.options.gbfsURL}station_status.json`);
+      const stationStatus = await stationStatusResponse.json();
 
-            stations.data.stations.forEach((station) => {
-                stationStatus.data.stations.forEach(status => {
-                    if ((status.station_id == station.station_id) && status.is_installed) {
-                        let icon = new L.DivIcon({
-                            html: this.getStationIconHtml(status.num_bikes_available, status.num_docks_available),
-                            bgPos: [13, 13],
-                            iconSize: [26, 26],
-                            popupAnchor: [0, -17],
-                            className: "bike-icon"
-                        });
-                        let point = L.latLng(station.lat, station.lon);
-                        let marker = new L.Marker(point, {
-                            icon: icon
-                        });
-                        marker.addTo(this.container)
-                    }
-                })
-            })
-        } catch(err) {
-            console.warn(err);
-            this.fire('error', { error: err });
-        }
+      this.container.clearLayers();
 
-        return this;
-    },
+      stations.data.stations.forEach((station) => {
+        stationStatus.data.stations.forEach((status) => {
+          if ((status.station_id === station.station_id) && status.is_installed) {
+            const icon = new L.DivIcon({
+              html: this.getStationIconHtml(status.num_bikes_available, status.num_docks_available),
+              bgPos: [13, 13],
+              iconSize: [26, 26],
+              popupAnchor: [0, -17],
+              className: 'bike-icon',
+            });
+            const point = L.latLng(station.lat, station.lon);
+            const marker = new L.Marker(point, {
+              icon,
+            });
+            marker.addTo(this.container);
+          }
+        });
+      });
+    } catch (err) {
+      console.warn(err);
+      this.fire('error', { error: err });
+    }
 
-    getBounds() {
-        if (this.container.getBounds) {
-            return this.container.getBounds();
-        }
+    return this;
+  },
 
-        throw new Error('Container has no getBounds method');
-    },
+  getBounds() {
+    if (this.container.getBounds) {
+      return this.container.getBounds();
+    }
 
-    onAdd(map) {
-        map.addLayer(this.container);
-        if (this.options.start) {
-            this.start();
-        }
-    },
+    throw new Error('Container has no getBounds method');
+  },
 
-    onRemove(map) {
-        if (this.options.onlyRunWhenAdded) {
-            this.stop();
-        }
-        
-        map.removeLayer(this.container);
-    },
+  onAdd(map) {
+    map.addLayer(this.container);
+    if (this.options.start) {
+      this.start();
+    }
+  },
 
-    getStationIconHtml(bikes, docks) {
-		let cssClass = "bike-icon-inner"
-		if (bikes == 0) {
-			cssClass += " bike-icon-empty"
-		}
-		let degree = bikes/(bikes+docks) * 360
-		//degree = 270
-		let ringCss = `
-		background: ${this.options.bikeMarkerColor};
-		background-image:
-			linear-gradient(${90+degree}deg, transparent 50%, ${this.options.bikeMarkerBgColor} 50%),
-			linear-gradient(90deg, ${this.options.bikeMarkerBgColor} 50%, transparent 50%);
-		`
-		if (degree > 180) {
-			ringCss = `
-			background: ${this.options.bikeMarkerColor};
-			background-image:
-				linear-gradient(${degree-90}deg, transparent 50%, ${this.options.bikeMarkerColor} 50%),
-				linear-gradient(90deg, ${this.options.bikeMarkerBgColor} 50%, transparent 50%);
-			`
-		}
-		return `
-		<div class="bike-icon-ring" style="${ringCss}">
-			<div class="${cssClass}">${bikes}</div>
-		</div>
-		`
-	}
+  onRemove(map) {
+    if (this.options.onlyRunWhenAdded) {
+      this.stop();
+    }
+
+    map.removeLayer(this.container);
+  },
+
+  getStationIconHtml(bikes, docks) {
+    let cssClass = 'bike-icon-inner';
+    if (bikes === 0) {
+      cssClass += ' bike-icon-empty';
+    }
+    const degree = (bikes / (bikes + docks)) * 360;
+    // degree = 270
+    let ringCss = `
+      background: ${this.options.bikeMarkerColor};
+      background-image:
+        linear-gradient(${90 + degree}deg, transparent 50%, ${this.options.bikeMarkerBgColor} 50%),
+        linear-gradient(90deg, ${this.options.bikeMarkerBgColor} 50%, transparent 50%);
+    `;
+
+    if (degree > 180) {
+      ringCss = `
+        background: ${this.options.bikeMarkerColor};
+        background-image:
+          linear-gradient(${degree - 90}deg, transparent 50%, ${this.options.bikeMarkerColor} 50%),
+          linear-gradient(90deg, ${this.options.bikeMarkerBgColor} 50%, transparent 50%);
+      `;
+    }
+    return `
+      <div class="bike-icon-ring" style="${ringCss}">
+        <div class="${cssClass}">${bikes}</div>
+      </div>
+    `;
+  },
 });
 
 module.exports = L.GBFS;
